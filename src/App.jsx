63673d04ffa2112e697d3fd2e914db6167340212
filipenadai <f.nadai05@gmail.com>
2022-06-25 17:50:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header";
+import './global.css';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <h1>
+        Hello
+      </h1>
+
+    </div>
+  )
+}
+
