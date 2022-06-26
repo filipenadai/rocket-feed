0fcc74858efbfafa7cb15d3f6b70ba59a82bad6f
filipenadai <f.nadai05@gmail.com>
2022-06-26 18:18:17 -0300

    add post and sidebar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { Header } from "./components/Header";
+
+import styles from './App.module.css'
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 export function App() {
   return (
     <div>
       <Header />
-      <h1>
-        Hello
-      </h1>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post />
+        </main>
+      </div>
 
     </div>
   )
